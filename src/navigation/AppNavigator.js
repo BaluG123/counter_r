077dd4f408c5436d87705fd6c88a36cf33b53f5a
Balappa <balappa.goudi@blockstack.tech>
2024-10-13 00:52:49 +0530

    app is almost ready

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,8 +1,8 @@
 // src/navigation/AppNavigator.tsx
 
-import React, { useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useState} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import ListScreen from '../screens/ListScreen';
 import DetailScreen from '../screens/DetailScreen';
 import Splashscreen from '../screens/Splashscreen';
@@ -14,33 +14,42 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={initialRoute}>
-        <Stack.Screen name="List" component={ListScreen}
-        options={{
-          headerTitle: 'Pray List', // Set title for List screen
-          headerTitleAlign: 'center', // Center the title
-          headerStyle: {
-            backgroundColor: '#f8f8f8', // Background color for the header
-          },
-          headerTitleStyle: {
-            fontSize: 20, // Font size for the title
-            color: '#333', // Color of the title
-          },
-        }} />
-        <Stack.Screen name="Detail" component={DetailScreen} 
-        options={{
-          headerLeft: null,
-          headerTitle: 'Pray Details', // Set a different title for Detail screen
-          headerTitleAlign: 'center', // Center the title
-          headerStyle: {
-            backgroundColor: '#f8f8f8', // Different background color for Detail header
-          },
-          headerTitleStyle: {
-            fontSize: 20, // Font size for the title
-            color: '#333', // Color of the title
-          },
-        }}/>
-        <Stack.Screen name="SplashScreen" component={Splashscreen}
-        options={{headerShown:false}} />
+        <Stack.Screen
+          name="List"
+          component={ListScreen}
+          options={{
+            headerTitle: 'Prayer Journal', // Set title for List screen
+            headerTitleAlign: 'center', // Center the title
+            headerStyle: {
+              backgroundColor: '#f8f8f8', // Background color for the header
+            },
+            headerTitleStyle: {
+              fontSize: 20, // Font size for the title
+              color: '#333', // Color of the title
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{
+            headerLeft: null,
+            headerTitle: 'Prayer Details', // Set a different title for Detail screen
+            headerTitleAlign: 'center', // Center the title
+            headerStyle: {
+              backgroundColor: '#f8f8f8', // Different background color for Detail header
+            },
+            headerTitleStyle: {
+              fontSize: 20, // Font size for the title
+              color: '#333', // Color of the title
+            },
+          }}
+        />
+        <Stack.Screen
+          name="SplashScreen"
+          component={Splashscreen}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
